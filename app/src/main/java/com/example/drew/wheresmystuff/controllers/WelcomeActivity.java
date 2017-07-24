4dba9b2eda7db5f1907d4d39925ba15d16af4993
Bathenape <jordan.leahey@gmail.com>
2017-07-24 16:17:38 -0400

    adding google+ sign in

--- app/src/main/java/com/example/drew/wheresmystuff/controllers/WelcomeActivity.java
+++ app/src/main/java/com/example/drew/wheresmystuff/controllers/WelcomeActivity.java
@@ -7,14 +7,25 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.drew.wheresmystuff.R;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class WelcomeActivity extends AppCompatActivity {
     Button registrationButton;
+
+    private GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
         registrationButton = (Button) findViewById(R.id.btnRegister);
         registrationButton.setOnClickListener(new View.OnClickListener() {
@@ -24,8 +35,14 @@ public class WelcomeActivity extends AppCompatActivity {
                 startActivity(registationActivity);
             }
         });
+
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
     }
 
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
 
     protected void onLoginPressed(View view) {
         Intent loginActivity = new Intent(this, LoginActivity.class);
