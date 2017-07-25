9152a0ef8d198bcc0d623630e134f1dc0cfe2c7a
jleahey <jordan.leahey@gmail.com>
2017-07-25 13:46:30 -0400

    added junit tests for UserManager

--- app/src/test/java/com/example/drew/wheresmystuff/WheresMyStuffTests.java
+++ app/src/test/java/com/example/drew/wheresmystuff/WheresMyStuffTests.java
@@ -3,6 +3,7 @@ package com.example.drew.wheresmystuff;
 import com.example.drew.wheresmystuff.controllers.GoogleMapViewHandler;
 import com.example.drew.wheresmystuff.model.ItemReport;
 import com.example.drew.wheresmystuff.model.User;
+import com.example.drew.wheresmystuff.model.UserManager;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -11,8 +12,10 @@ import org.junit.Test;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.*;
 
 /**
@@ -112,5 +115,35 @@ public class WheresMyStuffTests {
             assertEquals(itemReports[i].getReward(), "An A on this project");
         }
         // all tests passed
+    }
+
+    /**
+     * Jordan Leahey - jleahey3
+     * JUnit test to confirm that adding users to UserManager does not impact the user's information.
+     */
+    @Test
+    public void test_userManager() {
+        //using this array to store the users outside of the userManager class
+        ArrayList<User> uArray = new ArrayList<>(3);
+        //UserManager class used to store users and their emails
+        UserManager test = new UserManager();
+        //instantiating the users for testing
+        User test_user = new User("test", "test@test.com", "test", false);
+        User test_user2 = new User("test2", "test2@test.com", "test2", false);
+        User test_user3 = new User("test3", "test2@test.com", "test2", false);
+        //adding users to the array for comparison to UserManager
+        uArray.add(test_user);
+        uArray.add(test_user2);
+        uArray.add(test_user3);
+        //adding users to the Hashmap stored in UseManager
+        test.putUser("test@test.com", test_user);
+        test.putUser("test2@test.com", test_user2);
+        test.putUser("test3@test.com", test_user3);
+        //Comparing the original users to the ones in UserManger (test) to see if adding them
+        //to the test had any effect on the account information
+        assertEquals(uArray.get(0), test.getUser("test@test.com"));
+        assertEquals(uArray.get(1), test.getUser("test2@test.com"));
+        assertEquals(uArray.get(2), test.getUser("test3@test.com"));
+        //all tests passed
     }
 }
