3d7c8bd8530def64cfbe5b8c3200a32b8484bfe9
sofdavalos <sofia.davalos1906@gmail.com>
2017-07-25 14:09:44 -0400

    Created Espresso Test to check whether buttons on welcome screen are displayed correctly.

---
+++ app/src/androidTest/java/com/example/drew/wheresmystuff/controllers/WelcomeActivityTest.java
@@ -0,0 +1,83 @@
+package com.example.drew.wheresmystuff.controllers;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.example.drew.wheresmystuff.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class WelcomeActivityTest {
+
+    /**
+     * Sofia Davalos - sdavalos3
+     * JUnit Espresso test to check whether the Register and Login Buttons are displayed correctly
+     */
+
+    @Rule
+    public ActivityTestRule<WelcomeActivity> mActivityTestRule = new ActivityTestRule<>(WelcomeActivity.class);
+
+    @Test
+    public void welcomeActivityTest() {
+        // Checks Resgister button
+        ViewInteraction button = onView(
+                allOf(withId(R.id.btnRegister),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        button.check(matches(isDisplayed()));
+
+        // Checks Login button
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.btnLogin),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        button2.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
